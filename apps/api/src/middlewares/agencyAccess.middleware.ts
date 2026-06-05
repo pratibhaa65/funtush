ebdb43d63d205b6f4c28d946efb5c98756fda815
Dipika K.C <dipikak0323@gmail.com>
2026-06-05 22:07:17 +0545

    feat: middleware - custom domain name

--- apps/api/src/middlewares/agencyAccess.middleware.ts
+++ apps/api/src/middlewares/agencyAccess.middleware.ts
@@ -3,7 +3,6 @@ import type { AgencyRequest } from "../types/auth-request";
 
 
 export const checkAgencyStatus = (req: AgencyRequest, res: Response, next: NextFunction) => {
-
     // From auth middleware
     const agency = req.agencyUser;
 
@@ -18,6 +17,28 @@ export const checkAgencyStatus = (req: AgencyRequest, res: Response, next: NextF
         return res.status(403).json({
             success: false,
             message: "Account is locked. Please upgrade subscription for usage.",
+        });
+    }
+
+    next();
+};
+
+
+export const isPaidTier = (req: AgencyRequest, res: Response, next: NextFunction) => {
+
+    const agency = req.agencyUser;
+
+    if (!agency) {
+        return res.status(401).json({
+            success: false,
+            message: "Unauthorized",
+        });
+    }
+
+    if (agency.tier === "FREE") {
+        return res.status(403).json({
+            success: false,
+            message: "Custom domains are for the paid tiers. Please upgrade subscription.",
         });
     }
 
