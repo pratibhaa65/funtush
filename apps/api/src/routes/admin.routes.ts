0a006f2dc44479d006a367d3ab4eb1832fec4764
Dipika K.C <dipikak0323@gmail.com>
2026-06-12 13:08:46 +0545

    fix: conflict

--- apps/api/src/routes/admin.routes.ts
+++ apps/api/src/routes/admin.routes.ts
@@ -1,13 +1,13 @@
-import express from "express";
-import { approveAgencyKYC, rejectAgencyKYC } from "../controllers/agency.adminController.js";
+// import express from "express";
+// import { approveAgencyKYC, rejectAgencyKYC } from "../controllers/agency.adminController.js";
 
 
-const router = express.Router();
+// const router = express.Router();
 
-router.route("/admin/agencies/:id/kyc/approve")
-    .patch(approveAgencyKYC);
+// router.route("/admin/agencies/:id/kyc/approve")
+//     .patch(approveAgencyKYC);
 
-router.route("/admin/agencies/:id/kyc/reject")
-    .patch(rejectAgencyKYC);
+// router.route("/admin/agencies/:id/kyc/reject")
+//     .patch(rejectAgencyKYC);
 
-export default router;
+// export default router;
