912d9213b7f21f37f8e6aad423997fab7bc8c38d
Dipika K.C <dipikak0323@gmail.com>
2026-06-21 17:39:37 +0545

    feat: Review Moderation routing

--- apps/api/src/routes/admin.routes.ts
+++ apps/api/src/routes/admin.routes.ts
@@ -1,8 +1,10 @@
-// import express from "express";
+import express from "express";
+import { dismissReviewFlag, getFlaggedAgency, removeReview } from "src/controllers/admin.controller";
+import { requireAdmin } from "src/middleware/requireAdmin.middleware";
 // import { approveAgencyKYC, rejectAgencyKYC } from "../controllers/agency.adminController.js";
 
 
-// const router = express.Router();
+const router = express.Router();
 
 // router.route("/admin/agencies/:id/kyc/approve")
 //     .patch(approveAgencyKYC);
@@ -10,4 +12,13 @@
 // router.route("/admin/agencies/:id/kyc/reject")
 //     .patch(rejectAgencyKYC);
 
-// export default router;
+router.route("/admin/reviews/flagged")
+    .get(getFlaggedAgency);
+
+router.route("/admin/reviews/:id/remove")
+    .patch(requireAdmin, removeReview);
+
+router.route("/admin/reviews/:id/dismiss-flag")
+    .patch(requireAdmin, dismissReviewFlag);
+
+export default router;
