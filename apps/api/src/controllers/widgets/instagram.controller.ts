4005b5e381ca4cc800159344b2d6142b80dc0169
Dipika K.C <dipikak0323@gmail.com>
2026-08-01 16:44:00 +0545

    feat: Marketing/Analytics Widgets - instagram Oauth controller

--- apps/api/src/controllers/widgets/instagram.controller.ts
+++ apps/api/src/controllers/widgets/instagram.controller.ts
@@ -132,6 +132,7 @@ export const instagramCallbackController = async (
             message: "Instagram connected successfully."
         });
 
+<<<<<<< HEAD
     } catch (err) {
         return res.status(500).json({
             success: false,
@@ -139,6 +140,14 @@ export const instagramCallbackController = async (
                 err instanceof Error
                     ? err.message
                     : "Instagram OAuth failed."
+=======
+    } catch (error: any) {
+        return res.status(500).json({
+            success: false,
+            message:
+                error.response?.data?.error?.message ??
+                "Instagram OAuth failed."
+>>>>>>> 5f38f29 (feat: Marketing/Analytics Widgets - instagram Oauth controller)
         });
 
     }
