4d7ace9bdd76201b7044e0ccacb59e2200a5033f
Dipika K.C <dipikak0323@gmail.com>
2026-08-02 00:11:18 +0545

    feat: Utility Widgets - tier gate checking middleware

--- apps/api/src/middleware/tierGateCheck.middleware.ts
+++ apps/api/src/middleware/tierGateCheck.middleware.ts
@@ -31,11 +31,15 @@ export const tierGate =
                         id: agencyUser.agencyId,
                     },
                     include: {
+<<<<<<< HEAD
                         tier: {
                             select: {
                                 name: true
                             }
                         }
+=======
+                        tier: true,
+>>>>>>> 3163da2 (feat: Utility Widgets - tier gate checking middleware)
                     },
                 });
 
