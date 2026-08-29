c2de5bd5c587384177206edf0819dc85bf2a2756
Pratibha Sharma <pratibhaa625@gmail.com>
2026-08-29 18:37:44 +0545

    auth changes

--- apps/api/src/validations/auth.validation.ts
+++ apps/api/src/validations/auth.validation.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z.string().email("Invalid email format"),
+  email: z.string().trim().email("Invalid email format"),
   password: z.string().min(6, "Password must be at least 6 characters"),
 });
 
