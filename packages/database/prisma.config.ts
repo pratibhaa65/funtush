5886ce497fcc2f334cbc277bbd8449b952833fdc
Pratibha Sharma <pratibhaa625@gmail.com>
2026-09-02 17:47:06 +0545

    debug: log/ lint errors

--- packages/database/prisma.config.ts
+++ packages/database/prisma.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig, env } from "prisma/config";
 import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config({ path: "./.env" });
-
-console.log("DATABASE_URL =", process.env.DATABASE_URL);
+console.log("BEFORE dotenv:", process.env.DATABASE_URL);
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+console.log("AFTER dotenv:", process.env.DATABASE_URL);
 
 export default defineConfig({
   schema: "./prisma/schema.prisma",
